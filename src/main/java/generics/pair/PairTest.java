efb987f0e6024e57684178aaeeb26eb604413371
serghalak <serghalak@gmail.com>
2022-05-24 14:20:25 +0300

    add generics restriction <T extents Comparable>

--- src/main/java/generics/pair/PairTest.java
+++ src/main/java/generics/pair/PairTest.java
@@ -1,5 +1,7 @@
 package generics.pair;
 
+import java.util.Comparator;
+
 public class PairTest {
     public static void main(String[] args) {
         String[] words = {"Mary", "had", "a", "little", "lamd"};
@@ -17,11 +19,11 @@ public class PairTest {
 }
 
 class ArrayAlg {
-    public static Pair<String> minmax(String[] a) {
+    public static <T extends Comparable> Pair<T> minmax(T[] a) {
         if(a == null || a.length == 0)
             return null;
-        String min = a[0];
-        String max = a[1];
+        T min = a[0];
+        T max = a[1];
         for (int i = 1; i < a.length; i++) {
             if(min.compareTo(a[i]) > 0)
                 min = a[i];
@@ -33,5 +35,17 @@ class ArrayAlg {
 
     public static <T> T getMiddle(T ... a) {
         return a[a.length / 2];
+    }
+
+    public static <T extends Comparable> T min(T[] a) {
+
+        if (a == null || a.length == 0)
+            return null;
+        T smallest = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (smallest.compareTo(a[i]) > 0)
+                smallest = a[i];
+        }
+        return smallest;
     }
 }
